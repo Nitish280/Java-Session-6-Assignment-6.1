c73ea3b38aa2a71a68cb5284e405f04d37d9faaa
Nitish280 <nsrccs@gmail.com>
2017-06-08 12:19:42 +0530

    Class

---
+++ src/ClassA.java
@@ -0,0 +1,37 @@
+/**
+ * here i was taken four different classes but making all of the classes public
+ * is not possible we can make only one class as a public in single program
+ * After that by default we can create so many classes 
+ *  @author UNI
+ */
+public class ClassA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Heloo All");
+
+	}
+
+}
+
+class ClassB {
+
+	public static void main(String[] args) {
+		System.out.println("All good");
+
+	}
+
+}
+
+class ClassC {
+
+	public static void main(String[] args) {
+		System.out.println("All are Fine");
+	}
+}
+
+class ClassD {
+	public static void main(String[] args) {
+		System.out.println("All is Well");
+	}
+}
